edf68d034b5fa8a39b795f205724a0a880389b69
jo_kil <jo.kil.plays@gmail.com>
2021-12-09 22:15:22 +0100

    finished day3p1 and work on day2p1

--- days/day2p1/src/main.rs
+++ days/day2p1/src/main.rs
@@ -15,27 +15,50 @@ struct Instruction {
 
 fn parse_instructions(lines: Vec<String>) -> Vec<Instruction> {
     let instructions: Vec<Instruction> = Vec::new();
-    for line in lines {
-        let a: Vec<String> = line.split(" ").collect();
 
-        let instruction = line.split(" ").collect();
+    for line in lines {
+        let instruction_chunks: Vec<String> = line.split("something").map(str::to_string).collect();
+
+        let name = &instruction_chunks[0];
+        let amount = &instruction_chunks[1];
+
+        let instruction: Instruction = Instruction {
+            instruction_name: match name.to_uppercase().as_str() {
+                "UP" => InstructionTypes::UP,
+                "DOWN" => InstructionTypes::DOWN,
+                "FORWARD" => InstructionTypes::FORWARD,
+                _ => {
+                    println!("Something went wrong!");
+                    break;
+                }
+            },
+            amount: match amount.parse::<i64>() {
+                Ok(num) => num,
+                Err(_) => break,
+            },
+        };
+
+        instructions.push(Instruction {
+            amount: 10,
+            instruction_name: InstructionTypes::UP,
+        });
     }
 
     return instructions;
 }
 
-fn run_instructions(instructions: Vec<Instruction>) -> vec![i64, i64] {
-    let mut position: Vec<i64> = Vec::new();
+fn run_instructions(instructions: Vec<Instruction>) -> (i64, i64) {
+    let mut position: (i64, i64) = (0, 0);
     for instruction in instructions {
         match instruction.instruction_name {
             UP => {
-                position[1] += instruction.amount;
+                position.1 += instruction.amount;
             }
             DOWN => {
-                position[1] -= instruction.amount;
+                position.1 -= instruction.amount;
             }
             FORWARD => {
-                position[0] += instruction.amount;
+                position.0 += instruction.amount;
             }
         }
     }
--- days/day3p1/src/main.rs
+++ days/day3p1/src/main.rs
@@ -1,8 +1,47 @@
 use std::{fs, process::exit};
 
 fn main() {
-    println!("Hello, world!");
-    println!("{:?}", get_bits(&parse_input(&load_file_as_text("input.txt"))));
+    let bits_vec = get_bits(&parse_input(&load_file_as_text("input.txt")))
+        .iter()
+        .map(|e| e.to_vec())
+        .collect::<Vec<Vec<bool>>>();
+
+    let gamma_bits = [false; 12]
+        .iter()
+        .enumerate()
+        .map(|(i, _)| most_common_bit_in_column(&bits_vec, i as i64))
+        .collect::<Vec<bool>>();
+
+    let epsilon_bits = gamma_bits.iter().map(|e| !e).collect::<Vec<bool>>();
+
+    let gamma = bin_to_num(&gamma_bits);
+    let epsilon = bin_to_num(&epsilon_bits);
+
+    let power_consumption = gamma * epsilon;
+
+    print!("{:?}", power_consumption)
+}
+
+fn bin_to_num(bits: &Vec<bool>) -> u64 {
+    let mut num = 0;
+
+    for (i, bit) in bits.iter().rev().enumerate() {
+        if *bit {
+            num += 2_u64.pow(i as u32)
+        }
+    }
+
+    return num;
+}
+
+fn most_common_bit_in_column(bits_list: &Vec<Vec<bool>>, column: i64) -> bool {
+    let mut sum: u64 = 0;
+
+    for bits in bits_list {
+        sum += bits[column as usize] as u64;
+    }
+
+    return sum > (bits_list.len() as u64) / 2;
 }
 
 fn load_file_as_text(rel: &str) -> String {
@@ -17,19 +56,29 @@ fn parse_input(input: &String) -> Vec<String> {
         parsed_lines.push(line.trim().to_string());
     }
 
-    return parsed_lines;
+    return parsed_lines
+        .iter()
+        .cloned()
+        .filter(|s| s.len() != 0)
+        .collect::<Vec<String>>();
 }
 
 fn get_bits(lines: &Vec<String>) -> Vec<[bool; 12]> {
-    let bits_list: Vec<[bool; 12]> = Vec::new();
+    let mut bits_list: Vec<[bool; 12]> = Vec::new();
 
     for line in lines {
-        let digits: Vec<String> = line.split("").map(str::to_string).collect();
+        let digits = line
+            .trim()
+            .split("")
+            .map(str::to_string)
+            .filter(|s| s.len() != 0)
+            .collect::<Vec<String>>();
+
         let mut bits: [bool; 12] = [false; 12];
 
         for (i, digit) in digits.iter().enumerate() {
-
             if i > 11 {
+                println!("more digits than expected! ({})", digit);
                 break;
             }
 
@@ -37,13 +86,17 @@ fn get_bits(lines: &Vec<String>) -> Vec<[bool; 12]> {
                 "0" => false,
                 "1" => true,
                 "" => continue,
-                _ => {println!("Reading bits went wrong"); exit(1)}
+                _ => {
+                    println!("Reading bits went wrong");
+                    exit(1)
+                }
             };
 
             bits[i] = bit;
-            println!("{:?}", bits)
         }
+
+        bits_list.push(bits)
     }
 
-    return bits_list
+    return bits_list;
 }
