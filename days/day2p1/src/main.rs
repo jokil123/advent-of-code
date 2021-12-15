1af77487bc8cca5439f927e942760b2120d98923
JL <jo.kil.plays@gmail.com>
2021-12-15 16:15:37 +0100

    day2p2

--- days/day2p1/src/main.rs
+++ days/day2p1/src/main.rs
@@ -1,23 +1,52 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let pos = run_instructions(parse_instructions(parse_input(&load_file_as_text(
+        "input.txt",
+    ))));
+
+    println!("{:?}", pos);
+
+    println!("{:?}", pos.0 * pos.1);
 }
 
+#[derive(Debug)]
 enum InstructionTypes {
     UP,
     DOWN,
     FORWARD,
 }
 
+#[derive(Debug)]
 struct Instruction {
     instruction_name: InstructionTypes,
     amount: i64,
 }
 
-fn parse_instructions(lines: Vec<String>) -> Vec<Instruction> {
-    let instructions: Vec<Instruction> = Vec::new();
+fn load_file_as_text(rel: &str) -> String {
+    return fs::read_to_string(rel).expect("Something went wrong reading the file");
+}
+
+fn parse_input(input: &String) -> Vec<String> {
+    let mut parsed_lines: Vec<String> = Vec::new();
+    let lines: Vec<String> = input.split("\n").map(str::to_string).collect();
 
     for line in lines {
-        let instruction_chunks: Vec<String> = line.split("something").map(str::to_string).collect();
+        parsed_lines.push(line.trim().to_string());
+    }
+
+    return parsed_lines
+        .iter()
+        .cloned()
+        .filter(|s| s.len() != 0)
+        .collect::<Vec<String>>();
+}
+
+fn parse_instructions(lines: Vec<String>) -> Vec<Instruction> {
+    let mut instructions: Vec<Instruction> = Vec::new();
+
+    for line in lines {
+        let instruction_chunks: Vec<String> = line.split(" ").map(str::to_string).collect();
 
         let name = &instruction_chunks[0];
         let amount = &instruction_chunks[1];
@@ -38,27 +67,33 @@ fn parse_instructions(lines: Vec<String>) -> Vec<Instruction> {
             },
         };
 
-        instructions.push(Instruction {
-            amount: 10,
-            instruction_name: InstructionTypes::UP,
-        });
+        println!("{:?}", instruction);
+
+        instructions.push(instruction);
     }
+
+    println!("{}", instructions.len());
 
     return instructions;
 }
 
 fn run_instructions(instructions: Vec<Instruction>) -> (i64, i64) {
+    let mut aim: i64 = 0;
     let mut position: (i64, i64) = (0, 0);
     for instruction in instructions {
         match instruction.instruction_name {
-            UP => {
-                position.1 += instruction.amount;
+            InstructionTypes::UP => {
+                aim -= instruction.amount;
+                // position.1 -= instruction.amount;
             }
-            DOWN => {
-                position.1 -= instruction.amount;
+            InstructionTypes::DOWN => {
+                aim += instruction.amount;
+                // position.1 += instruction.amount;
             }
-            FORWARD => {
+            InstructionTypes::FORWARD => {
+                // position.0 += instruction.amount;
                 position.0 += instruction.amount;
+                position.1 += instruction.amount * aim;
             }
         }
     }
